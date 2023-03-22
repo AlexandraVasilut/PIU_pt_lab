742d367d8646b3fffc29643d61da5296c435c647
AlexandraVasilut <127866262+AlexandraVasilut@users.noreply.github.com>
2023-03-22 03:45:47 +0200

    Add files via upload

---
+++ ClasaCopil/ClasaCopil/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+class CopilClase
+{
+    public class Copii
+    {
+        private const char separator_fisier = ';';
+        private string nume;
+        private string prenume;
+        private string initiala_tata;
+        private string clasa;
+
+        public Copii(string n, string p, string ini, string cl) //Constructor
+        {
+            this.nume = n;
+            this.prenume = p;
+            this.initiala_tata = ini;
+            this.clasa = cl;
+        }
+
+        public Copii(string linieFisier)
+        {
+            var dateFisier = linieFisier.Split(separator_fisier);
+
+            nume = dateFisier[0];
+            prenume = dateFisier[1];
+            initiala_tata = dateFisier[2];
+            clasa = dateFisier[3]; 
+        }
+
+        public string Get_Nume() //get nume
+        {
+                return nume;
+        }
+        public string Get_Prenume() //get prenume
+        {
+                return prenume;
+        }
+
+        public string Get_InitialaTata()//get initiala tata
+        {
+            return initiala_tata;
+        }
+
+        public string Get_Clasa()//get clasa
+        {
+            return clasa;
+        }
+
+        public Copii()
+        {
+            nume = prenume = initiala_tata = clasa = string.Empty;
+        }
+
+        public string ConversieLaSir_Fisier()
+        {
+            string obiectCopiiPtFisier = string.Format("{1}{0}{2}{0}{3}{0}{4}{0}"),
+                separator_fisier, 
+                nume, 
+                prenume, 
+                initiala_tata, 
+                clasa);
+
+            return obiectCopiiPtFisier;
+        }
+    }
+    
+}
---
+++ ClasaCopil/Program/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Configuration;
+//using CopilClase;
+//using NivelStocareDate;
+
+namespace EvidentaCopii_Consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string opt;
+            do
+            {
+                Console.WriteLine("A. Adaugare copil de la tastatura.");
+                Console.WriteLine("B. Afisare copii.");
+                Console.WriteLine("C. Cautare copii dupa nume.");
+                Console.WriteLine("F. Afisare copii din fisier.");
+                Console.WriteLine("S. Salvare copii in fisier.");
+                Console.WriteLine("X. Inchidere program.");
+                Console.WriteLine("Alegeti o optiune: ");
+                opt = Console.ReadLine();
+                switch (opt.ToUpper())
+                {
+                    case "A":
+                        int idCopil = AdaugareCopilTastatura();
+
+                        break;
+
+                    case "B":
+                        string infoCopil = copil.Info();
+                        Console.WriteLine("Copilul {0}", infoCopil);
+
+                        break;
+
+                    case "C":
+                        Console.Write("Dati numele copilului pe care il cautati: ");
+                        string copil_cautat = Console.ReadLine();
+                        Copil[] lista = adminCopii.GetCopii(out nrCopii);
+                        cautareCopiiNume(lista, initiala_tata);
+
+                        break;
+
+                    case "F":
+                        Copil[] lista_copil = adminCopii.GetCopil(out nrCopii);
+                        AfisareCopii(lista_copii, nrCopii);
+
+                        break;
+
+                    case "S":
+                        idCopil = nrCopii + 1;
+                        nrCopii++;
+                        copil = new Copil(idCopil, "Ioana", "Radu");
+                        //adaugare student in fisier
+                        adminCopii.AddCopil(copil);
+
+                        break;
+                    case "X":
+
+                        return;
+                    default:
+                        Console.WriteLine("Optiunea nu exista!");
+
+                        break;
+                }
+            } while (opt.ToUpper() != "X");
+
+            Console.ReadKey();
+        }
+
+        //citire copii de la tastatura
+        public static Copil CitireCopilTastatura()
+        {
+            Console.WriteLine("Introdu numele copilului: ");
+            string nume = Console.ReadLine();
+            Console.WriteLine("Introdu prenumele copilului: ");
+            string prenume = Console.ReadLine();
+            Console.WriteLine("Introduceti initiala tatalui: ");
+            string initiala_tata = Console.ReadLine();
+            Console.WriteLine("Introduceti clasa din care face parte copilul: ");
+            string clasa = Console.ReadLine();
+            Copil copil = new Copil(nume, prenume, initiala_tata, clasa);
+
+            return copil;
+        }
+
+        //afisare copil
+        public static void AfisareCopil(Copil copil)
+        {
+            Console.WriteLine("Copilul este:");
+            string infoCopil = string.Format("Nume: {0}, Prenume: {1}, InitialaTata: {2}, Clasa: {3}",
+                   copil.Get_Nume() ?? " Necunoscut ",
+                   copil.Get_Prenume() ?? " Necunoscut ",
+                   copil.Get_InitialaTata() ?? " Necunoscut ",
+                   copil.GetClasa());
+
+            Console.WriteLine(infoCopil);
+            }
+        }
+
+    public static void AfisareCopii(Copii[] lista_copii, int nrCopii)
+    {
+        Console.WriteLine("Copiii sunt: ");
+        for (int contor = 0; contor < nrCopii; contor++)
+            AfisareCopii(lista_copii[contor]);
+    }
+}
+
