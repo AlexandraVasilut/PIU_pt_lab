650c0ab5c29bba048fcb845a6c431799e65e3a7b
AlexandraVasilut <127866262+AlexandraVasilut@users.noreply.github.com>
2023-04-12 15:16:47 +0300

    Add files via upload

---
+++ Copii/Copii/Copil.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modele 
+{
+
+    public class Copil
+    {
+        private const char separator_fisier = ';';
+        private int ID = 0;
+        private string nume;
+        private string prenume;
+        private string initiala_tata;
+        private string clasa;
+        private int idCopil; //identificator unic student
+
+        public Copil() //constructor implicit
+        {
+            nume = prenume = initiala_tata = clasa = string.Empty;
+        }
+
+        public Copil(int idCopil, string nume, string prenume, string initiala_tata, string clasa) //Constructor cu parametri
+        {
+            this.idCopil = idCopil;
+            this.nume = nume;
+            this.prenume = prenume;
+            this.initiala_tata = initiala_tata;
+            this.clasa = clasa;
+        }
+
+        //constructor cu un singur parametru de tip string care reprezinta o linie dintr-un fisier text
+        public Copil(string linieFisier)
+        {
+            var dateFisier = linieFisier.Split(separator_fisier);
+            //ordinea de preluare a campurilor este data de ordinea in care au fost scrise in fisier prin apelul implicit al metodei ConversieLaSir_PentruFisier()
+            idCopil = Convert.ToInt32(dateFisier[ID]);
+            nume = dateFisier[0];
+            prenume = dateFisier[1];
+            initiala_tata = dateFisier[2];
+            clasa = dateFisier[3];
+        }
+
+        public int Get_idCopil()
+        {
+            return idCopil;//get idCopil
+        }
+
+        public string Get_Nume() //get nume
+        {
+            return nume;
+        }
+        public string Get_Prenume() //get prenume
+        {
+            return prenume;
+        }
+
+        public string Get_InitialaTata()//get initiala tata
+        {
+            return initiala_tata;
+        }
+
+        public string Get_Clasa()//get clasa
+        {
+            return clasa;
+        }
+
+        public string ConversieLaSir_Fisier()
+        {
+            string obiectCopiiPtFisier = string.Format("{1}{0}{2}{0}{3}{0}{4}{0}",
+                separator_fisier,
+                idCopil.ToString(),
+                nume,
+                prenume,
+                initiala_tata,
+                clasa
+                    );
+
+            return obiectCopiiPtFisier;
+        }
+    }
+}
---
+++ Copii/Copii/Program.cs
@@ -0,0 +1,100 @@
+﻿using Modele;
+using System;
+using System.Configuration;
+using System.Globalization;
+
+namespace UI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Copil copil = new Copil();
+            //string numeFisier = ConfigurationManager.ApppSettings["NumeFisier"];
+            int nrCopii = 0; 
+            
+            string opt;
+            do
+            {
+                Console.WriteLine("A. Adaugare copil de la tastatura.");
+                Console.WriteLine("B. Afisare copii.");
+                //Console.WriteLine("C. Cautare copii dupa nume.");
+                Console.WriteLine("F. Afisare copii din fisier.");
+                Console.WriteLine("S. Salvare copil in fisier.");
+                Console.WriteLine("X. Inchidere program.");
+                Console.WriteLine("Alegeti o optiune: ");
+                opt = Console.ReadLine();
+                switch (opt.ToUpper())
+                {
+                    case "A":
+                        int idCopil = nrCopii + 1;
+                        Console.WriteLine("Introdu numele copilului: ", idCopil);
+                        string nume = Console.ReadLine();
+                        Console.WriteLine("Introdu prenumele copilului: ", idCopil);
+                        string prenume = Console.ReadLine();
+                        Console.WriteLine("Introduceti initiala tatalui: ", idCopil);
+                        string initiala_tata = Console.ReadLine();
+                        Console.WriteLine("Introduceti clasa din care face parte copilul: ", idCopil);
+                        string clasa = Console.ReadLine();
+                        copil = new Copil(idCopil, nume, prenume, initiala_tata, clasa);
+                        nrCopii++;
+
+                        break;
+
+                    case "B":
+                        string infoCopil = copil.Get_Nume();
+                        Console.WriteLine("Copilul {0}", infoCopil);
+
+                        break;
+
+                    /*case "C":
+                        Console.Write("Dati numele copilului pe care il cautati: ");
+                        string copil_cautat = Console.ReadLine();
+                        Copil[] lista = adminCopii.GetCopii(out nrCopii);
+                        cautareCopiiNume(lista, initiala_tata);
+
+                        break;*/
+
+                    /*case "F":
+                        Copil[] copii = adminCopii.GetCopii(out nrCopii);
+                        AfisareCopii(copii, nrCopii);
+
+                        break;
+
+                    /*case "S":
+                        idCopil = nrCopii + 1;
+                        nrCopii++;
+                        copil = new Copil(idCopil, "Ioana", "Radu");
+                        //adaugare student in fisier
+                        adminCopii.AddCopil(copil);
+
+                        break;*/
+
+                    case "X":
+                        return;
+                    default:
+                        Console.WriteLine("Optiunea nu exista!");
+
+                        break;
+                }
+            } while (opt.ToUpper() != "X");
+            Console.ReadKey();
+        }
+
+        //afisare copil
+        public static void AfisareCopil(Copil[] copii, int nrCopii)
+        {
+            Console.WriteLine("Copiii sunt:");
+            for (int contor = 0; contor < nrCopii; contor++)
+            {
+                string infoCopil = string.Format("Copilul cu id-ul #{0} are numele: {1}, Prenume: {2}, InitialaTata: {3}, Clasa: {4}",
+                    copii[contor].Get_idCopil(),               
+                    copii[contor].Get_Nume() ?? " NECUNOSCUT ",
+                    copii[contor].Get_Prenume() ?? " NECUNOSCUT ",
+                    copii[contor].Get_InitialaTata() ?? " NECUNOSCUT ",
+                    copii[contor].Get_Clasa() ?? " NECUNOSCUT ");
+                Console.WriteLine(infoCopil);
+            }
+        }
+    }
+}
